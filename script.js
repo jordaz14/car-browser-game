0e10504c80b20a6abb78097dd100c859d0581d59
Julian <1julianordaz@gmail.com>
2024-07-22 16:12:05 -0400

    Add js logic to randomize signs

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 console.log("Welcome to Traffic Game.");
 
 const playContainer = document.querySelector(".play-container");
-console.log(playContainer);
+
+const notifySign = document.querySelector(".notify-sign");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const signImage = document.createElement("img");
-signImage.src = "./assets/signs/image_13.png";
+
+signImage.src = `./assets/signs/image_${getRandomInt(0, 10)}.png`;
+
 signImage.alt = "Traffic sign";
 
 console.log(signImage);
 
 playContainer.appendChild(signImage);
+notifySign.appendChild(signImage);
