459de97e0e79e8f11518c3dd1c210248dfc260a6
Julian <1julianordaz@gmail.com>
2024-07-22 15:04:17 -0400

    Query and append img element via JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Welcome to Traffic Game.");
+
+const playContainer = document.querySelector(".play-container");
+console.log(playContainer);
+
+const signImage = document.createElement("img");
+signImage.src = "./assets/signs/image_13.png";
+signImage.alt = "Traffic sign";
+
+console.log(signImage);
+
+playContainer.appendChild(signImage);
