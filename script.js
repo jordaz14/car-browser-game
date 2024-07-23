9eede884bd0fba51100d614e528a2b8f90cef71d
Julian <1julianordaz@gmail.com>
2024-07-23 11:14:07 -0400

    Rename element counter to roundCounter

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ console.log(playContainer);
 
 const notifySign = document.querySelector(".notify-sign");
 const signImageNotify = document.createElement("img");
+const roundCounter = document.getElementById("roundCounter");
 //const signImagePlay = signImageNotify.cloneNode(true);
 notifySign.appendChild(signImageNotify);
 
-let notifySrc = '';
+let notifySrc = "";
 let numSigns = 10;
-var roundNumber = 0;
-
+let roundNumber = 0;
 
 //playContainer.appendChild(signImagePlay);
 function getRandomInt(min, max) {
@@ -21,61 +21,54 @@ function getRandomInt(min, max) {
 }
 
 function updateUI() {
-    newWaldo();
-    populateGame();
+  newWaldo();
+  populateGame();
 }
 
 function newWaldo() {
-    notifySrc = `./assets/signs/image_${getRandomInt(1, 515)}.png`;
-    signImageNotify.src = notifySrc;
-    //signImagePlay.src = newSrc;
-    signImageNotify.alt = "Traffic sign";
-
+  notifySrc = `./assets/signs/image_${getRandomInt(1, 515)}.png`;
+  signImageNotify.src = notifySrc;
+  //signImagePlay.src = newSrc;
+  signImageNotify.alt = "Traffic sign";
 }
 
 function populateGame() {
-    playContainer.innerHTML = '';
+  playContainer.innerHTML = "";
 
-    let imgList = [];
-    
-    //getting random image sources and appending to a list
-    let i = 0;
-    while (i < numSigns) {
-        let newSignSrc = `./assets/signs/image_${getRandomInt(1, 515)}.png`;
-        if (newSignSrc != notifySrc) {
-            imgList.push(newSignSrc);
-            i++;
-        }
+  let imgList = [];
+
+  //getting random image sources and appending to a list
+  let i = 0;
+  while (i < numSigns) {
+    let newSignSrc = `./assets/signs/image_${getRandomInt(1, 515)}.png`;
+    if (newSignSrc != notifySrc) {
+      imgList.push(newSignSrc);
+      i++;
     }
-    //add Waldo image
-    let notifyLoc = getRandomInt(0, numSigns)
-    imgList.splice(notifyLoc,0, notifySrc);
+  }
+  //add Waldo image
+  let notifyLoc = getRandomInt(0, numSigns);
+  imgList.splice(notifyLoc, 0, notifySrc);
 
-    //populate container
-    for (i = 0; i < imgList.length; i++){
-        const newSign = document.createElement("img");
-        newSign.src = imgList[i];
-        if (i == notifyLoc){
-            newSign.addEventListener('click', ()=> updateUI());
-        }
-        playContainer.appendChild(newSign);
+  //populate container
+  for (i = 0; i < imgList.length; i++) {
+    const newSign = document.createElement("img");
+    newSign.src = imgList[i];
+    if (i == notifyLoc) {
+      newSign.addEventListener("click", () => updateUI());
     }
+    playContainer.appendChild(newSign);
+  }
 
+  numSigns += 3;
 
-    numSigns += 3;
+  roundNumber++;
 
-    roundNumber++;
-    var element = document.getElementById("roundCounter");
-    element.textContent = "Round " + roundNumber;
-
+  roundCounter.textContent = `Round ${roundNumber}`;
 }
 
 //signImagePlay.addEventListener('click', ()=> updateUI());
 
 updateUI();
 
-
-
 //signImage.src = `./assets/signs/image_${getRandomInt(1, 515)}.png`;
-
-
