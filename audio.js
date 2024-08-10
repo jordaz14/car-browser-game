807951ed92f9b3ad03b2f34f6e470c3d8afdbb25
Julian <1julianordaz@gmail.com>
2024-08-09 23:39:05 -0400

    Add audio elements

--- audio.js
+++ audio.js
@@ -1,7 +1,40 @@
+import { gameState } from "./main.js";
+
+const muteButton = document.querySelector("#mute-button");
+muteButton.addEventListener("click", toggleMute);
+
+function toggleMute() {
+  // Set to mute
+  if (!gameState.muted) {
+    gameState.muted = true;
+    toggleAudio(gameState.active);
+    muteButton.textContent = "UNMUTE";
+    muteButton.style.backgroundColor = "white";
+    muteButton.style.color = "black";
+  }
+  // Set to unmute
+  else {
+    gameState.muted = false;
+    toggleAudio(gameState.active);
+    playSoundEffect("select");
+    muteButton.textContent = "MUTE";
+    muteButton.style.backgroundColor = "black";
+    muteButton.style.color = "white";
+  }
+}
+
 export function toggleAudio(gameActive) {
   const audio = document.querySelector("#audio-player");
 
-  gameActive == true ? audio.play() : audio.pause();
+  if (gameActive) {
+    if (!gameState.muted) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  } else {
+    audio.pause();
+  }
 }
 
 export function setAudioSpeed(speed) {
@@ -20,8 +53,19 @@ export function setAudioSpeed(speed) {
   }
 }
 
-export function playSoundEffect() {
-  const audio = new Audio("assets/one-beep.mp3");
+export function playSoundEffect(action) {
+  if (!gameState.muted) {
+    const audio = new Audio();
 
-  audio.play();
+    switch (action) {
+      case "select":
+        audio.src = "assets/select.mp3";
+        audio.play();
+        break;
+      case "death":
+        audio.src = "assets/death.mp3";
+        audio.play();
+        break;
+    }
+  }
 }
