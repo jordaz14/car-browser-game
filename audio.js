182f250765ddf953449123740729d4285cb671bb
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-10 14:42:00 -0400

    Add mute initializer function

--- audio.js
+++ audio.js
@@ -6,7 +6,9 @@ muteButton.addEventListener("click", toggleMute);
 function toggleMute() {
   // Set to mute
   if (!gameState.muted) {
+    console.log("muted");
     gameState.muted = true;
+    sessionStorage.setItem("muted", true);
     toggleAudio(gameState.active);
     muteButton.textContent = "UNMUTE";
     muteButton.style.backgroundColor = "white";
@@ -14,7 +16,9 @@ function toggleMute() {
   }
   // Set to unmute
   else {
+    console.log("unmuted");
     gameState.muted = false;
+    sessionStorage.setItem("muted", false);
     toggleAudio(gameState.active);
     playSoundEffect("select");
     muteButton.textContent = "MUTE";
@@ -23,10 +27,29 @@ function toggleMute() {
   }
 }
 
+export function muteInit() {
+  // If unmuted
+  console.log(gameState.muted);
+  console.log(!gameState.muted);
+  if (!gameState.muted) {
+    toggleAudio(gameState.active);
+    muteButton.textContent = "MUTE";
+    muteButton.style.backgroundColor = "black";
+    muteButton.style.color = "white";
+    // If muted
+  } else {
+    toggleAudio(gameState.active);
+    muteButton.textContent = "UNMUTE";
+    muteButton.style.backgroundColor = "white";
+    muteButton.style.color = "black";
+  }
+}
+
 export function toggleAudio(gameActive) {
   const audio = document.querySelector("#audio-player");
 
   if (gameActive) {
+    console.log(gameState.muted);
     if (!gameState.muted) {
       audio.play();
     } else {
@@ -37,18 +60,22 @@ export function toggleAudio(gameActive) {
   }
 }
 
-export function setAudioSpeed(speed) {
+export function setAudioSpeed() {
   const audio = document.querySelector("#audio-player");
+  const cachedDifficulty = sessionStorage.getItem("difficulty");
 
-  switch (speed) {
-    case "slow":
+  switch (cachedDifficulty) {
+    case "easy":
       audio.playbackRate = 0.75;
       break;
     case "normal":
       audio.playbackRate = 1;
       break;
-    case "fast":
+    case "hard":
       audio.playbackRate = 1.25;
+      break;
+    default:
+      audio.playbackRate = 1;
       break;
   }
 }
