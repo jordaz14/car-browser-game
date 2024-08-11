428f297d53d3712021a560bde28b4f35284650f0
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-11 10:52:06 -0400

    Create score object

---
+++ client/modules/score.js
@@ -0,0 +1,42 @@
+import { gameState } from "../main.js";
+
+export const score = {
+  active: {
+    el: document.querySelector("#active-score"),
+  },
+  high: {
+    el: document.querySelector("#high-score"),
+    score: sessionStorage.getItem("highScore"),
+
+    setHighScore() {
+      this.el.textContent = `HIGH SCORE: ${this.score}`;
+    },
+  },
+
+  updateScore(scoreInput) {
+    let activeScore = scoreInput;
+
+    switch (gameState.difficultyLevel) {
+      case "easy":
+        activeScore *= 1 / 2;
+        break;
+      case "normal":
+        activeScore;
+        break;
+      case "hard":
+        activeScore *= 2;
+        break;
+    }
+
+    activeScore = Math.round(activeScore);
+
+    score.active.el.textContent = `SCORE: ${activeScore}`;
+
+    if (activeScore > score.high.score) {
+      sessionStorage.setItem("highScore", activeScore);
+      score.high.el.textContent = `High Score: ${activeScore}`;
+    }
+  },
+};
+
+score.high.setHighScore();
