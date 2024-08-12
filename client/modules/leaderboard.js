fe552666b440645a8c9643910468f0bc7a93c3d6
Julian <1julianordaz@gmail.com>
2024-08-12 18:14:42 -0400

    Refresh leaderboard to pull from global results

--- client/modules/leaderboard.js
+++ client/modules/leaderboard.js
@@ -5,21 +5,29 @@ export function sayHi() {
 const url = "http://localhost:3000/";
 const tbody = document.querySelector("tbody");
 
-fetchData("").then((leaderboard) => {
-  console.log(leaderboard);
-  for (const entry of leaderboard) {
-    const newRow = document.createElement("tr");
-    for (const data in entry) {
-      if (data != "id") {
-        const newTableData = document.createElement("td");
-        newTableData.textContent = entry[data];
-        newRow.appendChild(newTableData);
-        console.log(entry[data]);
+function refreshLeaderboard() {
+  tbody.innerHTML = "";
+  let rowCounter = 0;
+  fetchData("").then((leaderboard) => {
+    for (const entry of leaderboard) {
+      const newRow = document.createElement("tr");
+      rowCounter++;
+      for (const data in entry) {
+        if (data != "id") {
+          const newTableData = document.createElement("td");
+          newTableData.textContent = entry[data];
+          newRow.appendChild(newTableData);
+        }
       }
+      const rankTableData = document.createElement("td");
+      rankTableData.textContent = rowCounter;
+      newRow.appendChild(rankTableData);
+      tbody.appendChild(newRow);
     }
-    tbody.appendChild(newRow);
-  }
-});
+  });
+}
+
+refreshLeaderboard();
 
 const partyForm = document.querySelector(".party-form");
 partyForm.addEventListener("submit", (e) => {
