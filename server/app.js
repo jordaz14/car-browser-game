0dcc6ce97c0440d0258f5ecc6c1975d1af223fe5
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-12 09:37:24 -0400

    Create connection to Neon db

--- server/app.js
+++ server/app.js
@@ -4,9 +4,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: "http://localhost:5501" }));
-
 app.use(express.json());
 
+const postgres = require("postgres");
+require("dotenv").config();
+
+// DB CONNECTION
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+PGPASSWORD = decodeURIComponent(PGPASSWORD);
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
+
+// ROUTE HANDLERS
 app.get("/join-party", (req, res) => {
   res.json({ message: "hello, world!" });
 });
