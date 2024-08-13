57558d10c018dae6450e7706a212e8856fb52b87
Julian <1julianordaz@gmail.com>
2024-08-13 15:18:08 -0400

    Add comments + status codes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const postgres = require("postgres");
 require("dotenv").config();
 
-// DB CONNECTION
+/* DB CONNECTION */
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 PGPASSWORD = decodeURIComponent(PGPASSWORD);
 
@@ -32,41 +32,52 @@ async function getPgVersion() {
 
 getPgVersion();
 
-// ROUTE HANDLERS
+/* ROUTE HANDLERS */
+
+// REFRESHES LEADERBOARD
 app.get("/refresh-leaderboard/:partyId", async (req, res) => {
   try {
+    // Extract requested party id
     const partyId = req.params.partyId;
-    console.log(partyId);
 
+    // Query usernames & scores for given party id
     const leaderboard = await sql`
     SELECT username, score 
     FROM leaderboard 
     WHERE party_id = ${partyId} 
     ORDER BY score DESC`;
 
-    res.json(leaderboard);
+    res.status(200).json({ message: "PARTY JOINED", data: leaderboard });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "INTERNAL SERVER ERROR" });
   }
 });
 
+// JOINS/CREATES PARTY
 app.post("/join-party", async (req, res) => {
   try {
-    const { party } = req.body;
+    // Extract name of party
+    let { party } = req.body;
 
+    party = party.toUpperCase();
+
+    // Query party id data for given party
     const partyData = await sql`
     SELECT party_id
     FROM party
     WHERE party_name = ${party}`;
 
+    // If party exists
     if (partyData.length > 0) {
-      console.log("result found!");
-      console.log(partyData);
+      // Isolate id number
       const partyId = partyData[0].party_id;
 
-      res.json(partyId);
-    } else {
+      res.json({ message: "PARTY FOUND", data: partyId });
+    }
+    // If party doesn't exist, create party
+    else {
+      // Create party in table
       await sql`
       INSERT INTO party (party_name) 
       VALUES (${party})`;
@@ -84,36 +95,51 @@ app.post("/join-party", async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "An error occurred." });
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
   }
 });
 
+// SUBMITS USER'S SCORE
 app.post("/submit-score", async (req, res) => {
   try {
+    // Extract req
     let { username, partyId, score } = req.body;
-    console.log(username, partyId, score);
 
+    // Standardize username format
     username = username.toUpperCase();
 
+    // Query for user data for given username
     const users = await sql`
-    SELECT username
+    SELECT username, score
     FROM leaderboard
     WHERE username = ${username} AND party_id = ${partyId}`;
 
+    // If user exists
     if (users.length > 0) {
-      await sql`UPDATE leaderboard
-      SET score = ${score} 
-      WHERE username = ${username} AND party_id = ${partyId}`;
-    } else {
+      // If submitted score > existing db score
+      if (score > users[0].score) {
+        // Update user score
+        await sql`
+        UPDATE leaderboard
+        SET score = ${score} 
+        WHERE username = ${username} AND party_id = ${partyId}`;
+        res.status(200).json({ message: "SCORE UPDATED" });
+      } else {
+        console.log("EXISTING SCORE EXCEEDS NEW SCORE");
+        res.status(200).json({ message: "EXISTING SCORE EXCEEDS NEW SCORE" });
+      }
+    }
+    // If user doesn't exist
+    else {
+      // Create user
       await sql`
       INSERT INTO leaderboard (username, party_id, score)
       VALUES (${username}, ${partyId}, ${score})`;
+      res.status(201).json({ message: "USER CREATED" });
     }
-
-    res.status(201).json({ message: "User added successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "An error occurred." });
+    res.status(500).json({ error: "INTERNAL SERVER ERROR" });
   }
 });
 
