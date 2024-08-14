31fa4f8619d31e38e6a5a8652a5f69d24c73a9ad
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-13 23:11:34 -0400

    Add leaderboard logic to tv

---
+++ client/rc/tv.js
@@ -0,0 +1,45 @@
+console.log("Hello world");
+
+const tbody = document.querySelector("tbody");
+const url = "https://traffic-browser-game-b0ad.onrender.com/";
+
+function refreshLeaderboard(partyId) {
+  tbody.innerHTML = "";
+  let rowCounter = 0;
+  fetchData("refresh-leaderboard", partyId).then((leaderboard) => {
+    console.log(leaderboard);
+    for (const entry of leaderboard.data) {
+      const newRow = document.createElement("tr");
+      rowCounter++;
+      for (const data in entry) {
+        if (data != "id") {
+          const newTableData = document.createElement("td");
+          newTableData.textContent = entry[data];
+          newRow.appendChild(newTableData);
+        }
+      }
+      const rankTableData = document.createElement("td");
+      rankTableData.textContent = rowCounter;
+      newRow.appendChild(rankTableData);
+      tbody.appendChild(newRow);
+    }
+  });
+}
+
+refreshLeaderboard(`/1`);
+
+async function fetchData(endpoint, param = "") {
+  try {
+    const response = await fetch(`${url}${endpoint}${param}`);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
