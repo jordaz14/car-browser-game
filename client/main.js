fee2afc3706c51aec76b5c4034340247febdca89
Julian <1julianordaz@gmail.com>
2024-08-19 18:44:14 -0400

    Remove difficulty logic from main.js & add comments

--- client/main.js
+++ client/main.js
@@ -1,202 +1,52 @@
+import { gameState } from "./gameState.js";
 import { helper } from "./modules/helper.js";
 import { motion } from "./modules/motion.js";
 import { audio } from "./modules/audio.js";
 import { environment } from "./modules/environment.js";
 import { score } from "./modules/score.js";
-import { init } from "./modules/leaderboard.js";
+import { difficulty } from "./modules/difficulty.js";
+import { leaderboard } from "./modules/leaderboard.js";
 
-export const gameState = {
-  active: false,
-  muted: helper.toBoolean(sessionStorage.getItem("muted")),
-  difficultyLevel: "normal",
-  movement: 2,
-  lastSpawnTime: 0,
-  spawnInterval: 4000,
-  animationFrameId: 0,
-};
-
-// HANDLES GAME DIFFICULTY (to be modularized)
-const difficulty = {
-  easy: {
-    el: document.querySelector("#easy-button"),
-    status: false,
-    movement: 1,
-    spawnInterval: 5000,
-    handleClick: function () {
-      difficulty.toggleDifficultyStatus(this);
-      difficulty.toggleDifficultyUI();
-      difficulty.setDifficultyGameState("easy", 1, 5000);
-      audio.playSoundEffect("select");
-      audio.setAudioSpeed();
-    },
-  },
-  normal: {
-    el: document.querySelector("#normal-button"),
-    status: true,
-    movement: 2,
-    spawnInterval: 4000,
-    handleClick: function () {
-      difficulty.toggleDifficultyStatus(this);
-      difficulty.toggleDifficultyUI();
-      difficulty.setDifficultyGameState("normal", 2, 4000);
-      audio.playSoundEffect("select");
-      audio.setAudioSpeed();
-    },
-  },
-  hard: {
-    el: document.querySelector("#hard-button"),
-    status: false,
-    movement: 3,
-    spawnInterval: 3000,
-    handleClick: function () {
-      difficulty.toggleDifficultyStatus(this);
-      difficulty.toggleDifficultyUI();
-      difficulty.setDifficultyGameState("hard", 3, 3000);
-      audio.playSoundEffect("select");
-      audio.setAudioSpeed();
-    },
-  },
-
-  // ADDS CLICK EVENT LISTENER TO EVERY DIFFICULTY LEVEL
-  addDifficultyEventHandler() {
-    for (const level in difficulty) {
-      if (difficulty[level].el) {
-        // Binds hanadle click to correct context of difficulty level
-        difficulty[level].boundHandleClick = difficulty[level].handleClick.bind(
-          difficulty[level]
-        );
-        difficulty[level].el.addEventListener(
-          "click",
-          difficulty[level].boundHandleClick
-        );
-      }
-    }
-  },
-
-  // REMOVES CLICK LISTENER FROM EVERY DIFFICULTY LEVEL
-  removeDifficultyEventHandler() {
-    for (const level in difficulty) {
-      if (difficulty[level].el) {
-        difficulty[level].el.removeEventListener(
-          "click",
-          difficulty[level].boundHandleClick
-        );
-      }
-    }
-  },
-
-  checkCachedDifficulty() {
-    const cachedDifficulty = sessionStorage.getItem("difficulty");
-    if (cachedDifficulty) {
-      for (const level in difficulty) {
-        if (difficulty[level].el) {
-          if (
-            difficulty[level].el.textContent.toLowerCase() == cachedDifficulty
-          ) {
-            difficulty[level].status = true;
-          } else {
-            difficulty[level].status = false;
-          }
-        }
-      }
-    }
-  },
-
-  // HANDLES DIFFICULTY STATUS STATE
-  toggleDifficultyStatus(difficultyLevel) {
-    for (const level in difficulty) {
-      if (difficulty[level] == difficultyLevel) {
-        difficulty[level].status = true;
-        sessionStorage.setItem(
-          "difficulty",
-          difficulty[level].el.textContent.toLowerCase()
-        );
-      } else if (difficulty[level].el) {
-        difficulty[level].status = false;
-      }
-    }
-  },
-
-  // HANDLES UI OF DIFFICULTY BUTTONS
-  toggleDifficultyUI() {
-    for (const level in difficulty) {
-      if (difficulty[level].status) {
-        difficulty[level].el.style.backgroundColor = "white";
-        difficulty[level].el.style.color = "black";
-      } else if (difficulty[level].el) {
-        difficulty[level].el.style.backgroundColor = "black";
-        difficulty[level].el.style.color = "white";
-      }
-    }
-  },
-
-  setDifficultyGameState() {
-    const cachedDifficulty = sessionStorage.getItem("difficulty");
-
-    if (cachedDifficulty) {
-      switch (cachedDifficulty) {
-        case "easy":
-          gameState.difficultyLevel = "easy";
-          gameState.movement = 1;
-          gameState.spawnInterval = 5000;
-          break;
-        case "normal":
-          gameState.difficultyLevel = "normal";
-          gameState.movement = 2;
-          gameState.spawnInterval = 4000;
-          break;
-        case "hard":
-          gameState.difficultyLevel = "hard";
-          gameState.movement = 3;
-          gameState.spawnInterval = 3000;
-          break;
-      }
-      return;
-    }
-
-    for (const level in difficulty) {
-      if (difficulty[level].status) {
-        gameState.difficultyLevel = level;
-        gameState.movement = difficulty[level].movement;
-        gameState.spawnInterval = difficulty[level].spawnInterval;
-      }
-    }
-  },
-};
-
-difficulty.addDifficultyEventHandler();
-difficulty.checkCachedDifficulty();
-difficulty.setDifficultyGameState();
-difficulty.toggleDifficultyUI();
-audio.muteInit();
-audio.setAudioSpeed();
-
-let startButton = document.querySelector(".game-status-sign");
+// SELECT GAME OPTIONS CONTAINER & START BUTTON
+let options = document.querySelector(".options");
+let startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", startGame);
-let gameCard = document.querySelector(".game-card");
 
+// START CAR GAME ON
 function startGame() {
   gameState.active = true;
-  checkScreenWidth(1200) ? (gameCard.style.display = "none") : null;
+
+  // Change start button to restart
+  createRestartButton(gameState.active);
+
+  // Hides game options if mobile
+  helper.checkScreenWidth(1200) ? (options.style.display = "none") : null;
+
+  // Restricts difficulty toggle in-game
   difficulty.removeDifficultyEventHandler();
+
+  // Enable user to move car
   motion.toggleUserInput(gameState.active);
-  toggleStatusSign(gameState.active);
+
+  // Plays music if not on mute
   audio.toggleAudio(gameState.active);
+
   gameLoop(gameState.active);
 }
 
+// LOOP THROUGH CAR GAME AT BROWSER FRAMERATE
 function gameLoop(timestamp) {
   if (gameState.active) {
-    // Enable user movement
+    // Moves car based on state of user input
     motion.moveUser(environment.car, environment.road);
 
-    // Checks spawning
+    // Spawn obstacles
     if (timestamp - gameState.lastSpawnTime > gameState.spawnInterval) {
       environment.createObstacle();
       gameState.lastSpawnTime = timestamp;
     }
 
-    // Enable obstacle and scenery movement
+    // Move abstacles and scenery
     motion.moveSceneObj(environment.activeObstacles, environment.road);
     motion.moveSceneObj(environment.activeScenery, environment.dirt.left);
 
@@ -205,8 +55,8 @@ function gameLoop(timestamp) {
       motion.collisionDetector(environment.car, environment.activeObstacles)
     ) {
       audio.playSoundEffect("death");
-      environment.car.el.className = "dead";
-      environment.car.el.src = "./assets/skull.png";
+      environment.car.el.className = "dead-car";
+      environment.car.el.src = "./assets/img/skull.png";
       stopGame();
     }
 
@@ -216,16 +66,27 @@ function gameLoop(timestamp) {
   }
 }
 
+// END CAR GAME
 function stopGame() {
   gameState.active = false;
-  checkScreenWidth(1200) ? (gameCard.style.display = "flex") : null;
+
+  // Show game options if mobile
+  helper.checkScreenWidth(1200) ? (options.style.display = "flex") : null;
+
+  // Restrict user from moving car
   motion.toggleUserInput(gameState.active);
-  toggleStatusSign(gameState.active);
+
+  // End game music if unmuted
   audio.toggleAudio(gameState.active);
 }
 
-//HANDLES UI OF START BUTTON
-function toggleStatusSign(gameActive) {
+// RESET CAR GAME BY PAGE RELOAD
+function resetGame() {
+  window.location.reload();
+}
+
+// CHANGE START BUTTON TO RESTART BUTTON
+function createRestartButton(gameActive) {
   if (gameActive) {
     startButton.textContent = `RESTART`;
     startButton.removeEventListener("click", startGame);
@@ -233,16 +94,5 @@ function toggleStatusSign(gameActive) {
   }
 }
 
-function resetGame() {
-  window.location.reload();
-}
-
-function checkScreenWidth(inputWidth) {
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth < inputWidth) {
-    return true;
-  } else {
-    return false;
-  }
-}
+//Initialize leaderboard.js
+leaderboard();
